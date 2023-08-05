d1624f994a05ce65d11992adc9076c90f47d033c
hello-world-ttj <tijotjoseph003@gmail.com>
2023-08-05 19:46:26 +0530

    #controllers for image fetching

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ const volleyball = require("volleyball");
 const app = express();
 app.use(cors());
 app.use(volleyball);
-const db = require("./helpers/connection/connection");
 const response = require("./helpers/response/response");
+const profile_controller = require("./controller/profile");
+const banner_controller = require("./controller/banner");
+
 app.use(express.json());
 const version = process.env.API_VERSION;
 
 app.get(`/api/${version}/`, (req, res) => {
   response(res, 200, `API testing`, null);
 });
+
+app.get(`/api/${version}/profile`, profile_controller);
+
+app.get(`/api/${version}/banner`, banner_controller);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on ${process.env.PORT}`);
